f2cc39a0529b4640959b35b52a7e40040ff4f546
Moses Oyewole <mmosix@gmail.com>
2025-02-02 00:15:53 +0100

    `Update admin projects page to use new autoloader and database class`

--- public/admin/projects.php
+++ public/admin/projects.php
@@ -1,8 +1,10 @@
 <?php
-require_once __DIR__.'/../../app/Auth.php';
-require_once __DIR__.'/../../app/Project.php';
+require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__ . '/../../app/Database.php';
+require_once __DIR__ . '/../../app/Auth.php';
 
-Auth::checkRole(['admin']);
+// Check if user is logged in and has admin role
+\App\Auth::checkRole(['admin']);
 
 $projects = Project::getAll();
 ?>
