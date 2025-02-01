ed926eb0777103d94ee8e3705a9e51a88924bd8e
Moses Oyewole <mmosix@gmail.com>
2025-02-01 19:18:12 +0100

    security

---
+++ app/Security.php
@@ -0,0 +1,27 @@
+<?php
+
+class Security {
+    /**
+     * Basic security functionality 
+     */
+    public static function sanitizeInput($input) {
+        if (is_array($input)) {
+            return array_map([self::class, 'sanitizeInput'], $input);
+        }
+        return htmlspecialchars(strip_tags(trim($input)), ENT_QUOTES, 'UTF-8');
+    }
+
+    public static function validateToken($token) {
+        if (!isset($_SESSION['csrf_token']) || $token !== $_SESSION['csrf_token']) {
+            return false;
+        }
+        return true;
+    }
+
+    public static function generateToken() {
+        if (!isset($_SESSION['csrf_token'])) {
+            $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+        }
+        return $_SESSION['csrf_token'];
+    }
+}
